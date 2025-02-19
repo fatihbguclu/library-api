5588b56cbc428c020896dedbddf395da4ca48bd9
fatihbguclu <curaat79@gmail.com>
2025-02-19 13:20:04 +0300

    first controller

---
+++ src/main/java/com/ft/library/controller/v1/BookController.java
@@ -0,0 +1,24 @@
+package com.ft.library.controller.v1;
+
+import com.ft.library.model.entity.Book;
+import com.ft.library.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBook() {
+        return bookService.getAllBook();
+        //return List.of(new Book());
+    }
+}
---
+++ src/main/java/com/ft/library/service/impl/BookServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ft.library.service.impl;
+
+import com.ft.library.model.entity.Book;
+import com.ft.library.repository.BookRepository;
+import com.ft.library.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    @Override
+    public List<Book> getAllBook() {
+        return bookRepository.findAll();
+    }
+}
