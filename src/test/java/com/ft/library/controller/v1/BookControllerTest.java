5bcbbdb980e6c0580c1b4ed4f893c504f973263c
fatihbguclu <curaat79@gmail.com>
2025-02-22 21:41:41 +0300

    book controller unit test shouldReturnAllBook

---
+++ src/test/java/com/ft/library/controller/v1/BookControllerTest.java
@@ -0,0 +1,43 @@
+package com.ft.library.controller.v1;
+
+import com.ft.library.model.entity.Book;
+import com.ft.library.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest(BookController.class)
+public class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private BookService bookService;
+
+    @Test
+    void shouldReturnAllBook() throws Exception {
+        when(bookService.getAllBook())
+                .thenReturn(
+                        List.of(
+                                Book.builder().title("asd").author("asd").build(),
+                                Book.builder().title("asd1").author("asd1").build()
+                        )
+                );
+
+        mockMvc.perform(get("/v1/books"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(2))
+                .andExpect(jsonPath("$[0].title").value("asd"));
+    }
+
+}
